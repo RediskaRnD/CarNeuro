0ce2f52e76b036daa6112726299a2e2d922325d8
Rediska <andrej.balakin@gmail.com>
2019-05-29 20:09:31 +0200

    добавили Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        clean: {
+            js: ['src/main/resources/**/*.js',
+                'src/main/resources/**/*.map',
+                '!src/main/resources/**/require.js']
+        },
+        ts: {
+            default : {
+                options: {
+                    rootDir: 'src/main/resources/static'
+                },
+                tsconfig: 'src/main/resources/tsconfig.json'
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.registerTask("default", ["clean", "ts"]);
+};
