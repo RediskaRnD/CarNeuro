74a39f8e1aeb9bae6c9073678b302ca8bcf013d2
Justinas <jusski@sdf-eu.org>
2019-05-30 11:54:13 +0300

    added javascript bridge sprintf to java, encodings UTF-8 jsweet plugin

---
+++ src/main/resources/static/def/sprintf/Globals.d.ts
@@ -0,0 +1,23 @@
+/* Generated from Java with JSweet 2.3.0-SNAPSHOT - http://www.jsweet.org */
+/**
+ * Example: <code>sprintf(
+ * "The first 4 letters of the English alphabet are: %s, %s, %s and %s",
+ * "a", "b", "c", "d")</code>
+ * @param {string} format
+ * @param {Array} args
+ * @return {string}
+ */
+export declare function sprintf(format : string, ...args : any[]) : string;
+
+/**
+ * Example: <code>vprintf(
+ * "The first 4 letters of the English alphabet are: %s, %s, %s and %s", new
+ * String[] {"a", "b", "c", "d"})</code>
+ * @param {string} format
+ * @param {Array} args
+ * @return {string}
+ */
+export declare function vprintf(format : string, args : any[]) : string;
+
+
+
